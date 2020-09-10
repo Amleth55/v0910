9ec6b6242e0a901d6fbe80d306556fcf239b5af4
ozeki <hhdfgg@users.noreply.github.com>
2020-09-10 09:39:09 +0900

    Initial commit

---
+++ v0910/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0910
+{
+    public partial class Form1 : Form
+    {
+        int[] vx = new int[3];
+        int[] vy = new int[3];
+        static Random rand = new Random();
+        Label[] labels = new Label[30];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 3; i++)
+            {
+                vx[i] = rand.Next(-15, 16);
+                vy[i] = rand.Next(-15, 16);
+
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "★";
+                Controls.Add(labels[i]);
+                labels[i].Left = rand.Next(ClientSize.Width - labels[i].Width);
+                labels[i].Top = rand.Next(ClientSize.Height - labels[i].Height);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            
+
+        }
+    }
+}
